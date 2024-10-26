50e5190c094ec251e7ef0b1b7b521605deaae4f0
projectnum17 <egor.chornyy@gmail.com>
2024-10-26 17:17:21 +0300

    mob layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
-
+    let menuBtn = document.querySelector('.header__burger');
+    
+    menuBtn.addEventListener('click', function () {
+        menuBtn.classList.toggle('opened');
+    });
     const mainPageHeroSlider = new Swiper('.hero__preview', {
         effect: 'fade',
         fadeEffect: {
@@ -45,17 +49,50 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         allowTouchMove: false,
         slidesPerView: 2.2,
+        breakpoints: {
+            992: {
+                spaceBetween: 20,
+                slidesPerView: 2.2,
+            },
+            768: {
+                spaceBetween: 15,
+                slidesPerView: 1.8,
+            },
+            767: {
+                slidesPerView: 1.5,
+                spaceBetween: 8,
+            },
+            320: {
+                spaceBetween: 8,
+                slidesPerView: 1.5,
+            },
+        },
     });
     
     const mainPageCardsSlider = new Swiper('.gallery__cards-items', {
         spaceBetween: 20,
         grabCursor: true,
         loop: true,
-        speed: 3000,
+        speed: 1500,
         slidesPerView: 3.2,
         navigation: {
             nextEl: '.gallery__cards .btn--next',
             prevEl: '.gallery__cards .btn--prev',
+        },
+        breakpoints: {
+            992: {
+                spaceBetween: 20,
+                slidesPerView: 3.2,
+            },
+            768: {
+                slidesPerView: 2.7,
+            },
+            767: {
+                slidesPerView: 1.2,
+            },
+            320: {
+                slidesPerView: 1.2,
+            },
         },
     });
 });
